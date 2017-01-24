cb2fd0559ef69166919d13662e864282e73f8d72
zflion <keyzf01@163.com>
2017-01-24 17:46:49 +0800

    add test code for websocket

---
+++ NODEJS/WebSocket/SimpleReqRes/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var wsio = require('websocket.io');
+
+var app = express.createServer();
+
+//attach websocket server
+var ws = wsio.attach(app);
+
+app.use(express.static('public'));
+
+ws.on('connection',function(socket){
+	socket.on('message',function(msg){
+		console.log(msg);
+		socket.send('pong');
+	});
+});
+
+app.listen(8000);
