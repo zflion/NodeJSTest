688401643a1d76f51508528e4ddd41919bcd785d
zflion <keyzf01@163.com>
2017-02-14 17:23:37 +0800

    small chat program using Socket.IO

--- nodejs/websocket/socket.io.chat/public/chat.js
+++ nodejs/websocket/socket.io.chat/public/chat.js
@@ -2,14 +2,57 @@ window.onload = function(){
 	var socket = io.connect();
 
 	socket.on('connect',function(){
-		socket.emit('join',prompt('What is your nickname?'));
+		var nickname = '';
+		while(nickname.length == 0){
+			nickname = prompt('What is your nickname?');
+		}
+		socket.emit('join',nickname);
+		document.getElementById('mynickname').innerHTML = nickname;
 		document.getElementById('chat').style.display= 'block';
 		
-		socket.on('announcement',function(msg){
+		
+		socket.on('announcement',function(newJoin,all){
+			document.getElementById('users').innerHTML = 'current in the chat:' + all;
+			
 			var li = document.createElement('li');
 			li.className = 'announcement';
-			li.innerHTML = msg;
+			li.innerHTML = newJoin + ' join the chat';
 			document.getElementById('messages').appendChild(li);
 		});
+		
+		
+	});
+	
+	function addMessage(from, text){
+		var li = document.createElement('li');
+		li.className = 'message';
+		li.innerHTML = '<b>' + from + '</b>:'+ text;
+		document.getElementById('messages').appendChild(li);
+	};
+	
+	var msgInput = document.getElementById('input');
+	document.getElementById('form').onsubmit = function(){
+		addMessage('me', msgInput.value);
+		socket.emit('text',input.value);
+		
+		input.value = '';
+		input.focus();
+		
+		return false;
+	};
+	
+	socket.on('text',addMessage);
+	
+	socket.on('userchange',function(all){
+			document.getElementById('users').innerHTML = 'current in the chat:' + all;	
+	});
+	
+	socket.on('userquit',function(nickname,remains){
+		document.getElementById('users').innerHTML = 'current in the chat:' + remains;	
+		
+		var li = document.createElement('li');
+		li.className = 'announcement';
+		li.innerHTML = nickname + ' left the chat';
+		document.getElementById('messages').appendChild(li);
 	});
 }
--- nodejs/websocket/socket.io.chat/server.js
+++ nodejs/websocket/socket.io.chat/server.js
@@ -14,15 +14,54 @@ io.sockets.on("connection",function(socket){
 	console.log("someone connected,current client:"+ clientKeys.length);
 	console.log(clientKeys);
 	socket.on('join',function(name){
+		var nicknames = new Array();
+		nicknames.push(name);
 		for (var i=0;i<clientKeys.length;++i){
 			var client = io.sockets.connected[clientKeys[i]];
-			if (client && client.nickname == name){
-				console.log("nickname existing");
-				// emit nickname exist event
+			
+			if (client){
+				if (client.nickname){
+					nicknames.push(client.nickname);
+				}
+				if (client.nickname == name){
+					console.log("nickname existing");
+				
+					// emit nickname exist event
+				}
 			}
 		}
 		socket.nickname = name;
-		socket.broadcast.emit('announcement',name + ' join the chat');
+		console.log(nicknames.join());
+		socket.broadcast.emit('announcement',name,nicknames.join());
+		socket.emit('userchange',nicknames.join());
 	});
 	
+	// disconnect
+	socket.on('disconnect',function(){
+		
+		if (socket.nickname){
+			var remains = getUserNicknames();
+			
+			socket.broadcast.emit('userquit',socket.nickname,remains.join());
+		}
+		console.log("someone disconnected: " + socket.nickname);
+	});
+	
+	// text
+	socket.on('text',function(msg){
+		socket.broadcast.emit('text',socket.nickname,msg);
+	});
 });
+
+// get nicknames for the current connected user
+function getUserNicknames(){
+	var clientKeys = Object.keys(io.sockets.connected);
+	var nicknames = new Array();
+	for (var i=0;i<clientKeys.length;++i){
+		var client = io.sockets.connected[clientKeys[i]];
+		if (client && client.nickname){				
+			nicknames.push(client.nickname);
+		}
+	}
+	return nicknames;
+};
